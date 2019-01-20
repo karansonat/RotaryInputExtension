81eee1c043d8f67f49331ea38b22b7dc77240838
Sonat Karan <sonatkaran@hotmail.com>
2019-01-20 17:08:23 +0300

    RotaryExtension

---
+++ Assets/RotaryExtension/Sample/SampleScript.cs
@@ -0,0 +1,19 @@
+﻿using RotaryInput;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleScript : MonoBehaviour
+{
+    public Text TextRotaryDirection;
+
+    private void Awake()
+    {
+        Debug.Log("Awake");
+        Debug.Log(RotaryInputExtension.GetBezelInput().ToString());
+    }
+
+    private void Update()
+    {
+        TextRotaryDirection.text = RotaryInputExtension.GetBezelInput().ToString();
+    }
+}
---
+++ Assets/RotaryExtension/Scripts/RotaryInputExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RotaryInput
+{
+    public enum RotaryDirection
+    {
+        None = 0,
+        Clockwise,
+        CounterClockwise
+    }
+
+    class RotaryInputExtension
+    {
+        [DllImport("rotaryinput")]
+        private static extern int getRotaryDirection();
+
+        public static RotaryDirection GetBezelInput()
+        {
+            return (RotaryDirection)getRotaryDirection();
+        }
+    }
+}
